e7fca14bae3ccdddd3df9aa5d80d044b18edabfe
zahra ove <zahra.oveyedzade@gmail.com>
2024-05-23 11:17:47 +0330

    add livewire

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Livewire\Counter;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/counter', Counter::class);
